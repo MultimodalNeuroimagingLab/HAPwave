cf4db0df60ddd10b41e751b456540aee0d618ea9
dorahermes <dorahermes@gmail.com>
2023-05-22 17:16:44 -0500

    updated code for signatures

--- pcc_fig5_signatureShift.m
+++ pcc_fig5_signatureShift.m
@@ -3,121 +3,83 @@ clearvars, close all, clc
 startup
 
 %% Plot figure showing how positive and negative peaks come from rec electrodes in different depths
-addpath(genpath(pwd));
+
+% Dependencies: MNL ieeg basics repository
+% cd to HAPwave repository
+
+addpath(genpath(pwd))
 
 % set local path to your BIDS directory:
 myPath = setLocalDataPath(1);
 localDataPath = myPath.input;
 
 % load the meta data
-all_subjects = {'01','02','03','04','05','06','07'}; % S4 & S5 have temporal delay
-all_hemi = {'r','r','r','l','r','l','l'};
-all_runs = {'01','01','01','01','01','01','01',};
-% all_out = [];
+all_subjects = {'01','02','03','04','05','06','07','08'}; % 
+all_hemi = {'r','r','r','l','r','l','l','r'};
+all_runs = {'01','01','01','01','01','01','01','01'};
 
-t_win_cod = [0.015 .5];
-
-for ss = 4%:length(all_subjects)
+% load the meta data
+for ss = 1:length(all_subjects)
     bids_sub = all_subjects{ss};
     bids_ses = 'ieeg01';
     bids_task = 'ccep';
     bids_run = all_runs{ss};
 
-    % set var names: mefd, events, channels and electrodes
-    fileName = fullfile(localDataPath,['sub-' bids_sub],['ses-' bids_ses],'ieeg',...
-        ['sub-' bids_sub '_ses-' bids_ses '_task-' bids_task '_run-' bids_run '_ieeg.mefd']);
-    events_tsv_name = fullfile(localDataPath,['sub-' bids_sub],['ses-' bids_ses],'ieeg',...
-        ['sub-' bids_sub '_ses-' bids_ses '_task-' bids_task '_run-' bids_run '_events.tsv']);
-    channels_tsv_name = fullfile(localDataPath,['sub-' bids_sub],['ses-' bids_ses],'ieeg',...
-        ['sub-' bids_sub '_ses-' bids_ses '_task-' bids_task '_run-' bids_run '_channels.tsv']);
-    electrodes_tsv_name = fullfile(localDataPath,['sub-' bids_sub],['ses-' bids_ses],'ieeg',...
-        ['sub-' bids_sub '_ses-' bids_ses '_electrodes.tsv']);
+    [events_table,channels_table,electrodes_table,sub_out] = pcc_loadAveragesStats(localDataPath,bids_sub,bids_ses,bids_task,bids_run);
+    all_out(ss) = sub_out;
+end
 
-    % load events, channels and electrodes
-    [metadata] = readMef3(fileName); % metadata table
-    events_table = readtable(events_tsv_name,'FileType','text','Delimiter','\t','TreatAsEmpty',{'N/A','n/a'}); % events table
-    channels_table = readtable(channels_tsv_name,'FileType','text','Delimiter','\t','TreatAsEmpty',{'N/A','n/a'}); % channels table
-    electrodes_table = readtable(electrodes_tsv_name,'FileType','text','Delimiter','\t','TreatAsEmpty',{'N/A','n/a'}); % electrodes table
-    % find good sEEG channels
-    good_channels = find(ismember(channels_table.type,{'ECOG','SEEG'}) & ismember(channels_table.status,'good'));
+%% Correct P values for number of comparisons in each subject
+area_codes = {[12123 53 54 12108 12109 12110 12106 12107 11123 59 17 18 11108 11109 11110 11106 11107 10]}; % all areas
 
-    % load stats
-    outputName = fullfile(localDataPath,'derivatives','stats',['sub-' bids_sub],...
-        ['sub-' bids_sub '_ses-' bids_ses '_task-' bids_task '_run-' bids_run '_stats.mat']);
-    load(outputName,'average_ccep','average_ccep_names','tt','srate','cross_ccep_t','cross_ccep_p');
+nr_subs = length(all_subjects);
 
-    % FDR correction for significance
-    cross_ccep_sig = NaN(size(cross_ccep_p));
-    [temp_ccep_sig,p_th] = ccepPCC_fdr(cross_ccep_p(good_channels,:),0.05);
-    cross_ccep_sig(good_channels,:) = temp_ccep_sig;
+out = []; % this will be a area X area structure, with all subjects concatinates for each area
+
+subj_resp_total = zeros(nr_subs,1);               % stim-->measured pair for stats FDR correction
+
+resp_counter = 0; % counting all responses across subjects for this connection
+
+for ss = 1:nr_subs % subject loop
+   
+    % Get sites that belong to the measurement ROI (rec_area)
+    these_measured_sites = find(ismember(all_out(ss).channel_areas,area_codes{1}));
     
-    all_out(ss).average_ccep = average_ccep;
-    all_out(ss).average_ccep_names = average_ccep_names;
-    all_out(ss).tt = tt;
-    all_out(ss).srate = srate;
-    all_out(ss).cross_ccep_t = cross_ccep_t;
-    all_out(ss).cross_ccep_p = cross_ccep_p;
-    all_out(ss).sig_ccep = cross_ccep_sig;
-     
-%     label areas of stim and measured electrodes
-    % number of channels
-    all_out(ss).nr_channels = height(channels_table);
-    % list of channel names
-    all_out(ss).channel_names = channels_table.name;
-    % find good sEEG/ECoG channels
-    all_out(ss).good_channels = find(ismember(channels_table.type,{'ECOG','SEEG'}) & ismember(channels_table.status,'good'));
-    all_out(ss).bad_channels = find(~ismember(channels_table.type,{'ECOG','SEEG'}) | ~ismember(channels_table.status,'good'));
-%     which hemisphere are we interested
-%     hemi = 'r'
-    hemi = all_hemi{ss};
-    if hemi == 'l'
-     areas_interest_L = [17 18 11106 11107 11108 11109 11110 11123 10];
-     area_names = {'hippocampus','amygdala','ant cing','mid ant cing','mid post cing','post dorsal cing','post ventral cing','parahippocampal gyrus', 'thalamus'};
-       channel_areas = zeros(size(all_out(ss).channel_names));
-        stim_areas = zeros(length(all_out(ss).average_ccep_names),2);
-        % loop through channel names to get channel areas
-        for kk = 1:length(all_out(ss).channel_names)
-            % which number has this channel in the electrodes.tsv file?
-            thisElPos = find(ismember(electrodes_table.name,all_out(ss).channel_names{kk}));
-            if ismember(electrodes_table.Destrieux_label(thisElPos),areas_interest_L)
-                channel_areas(kk) = find(ismember(areas_interest_L,electrodes_table.Destrieux_label(thisElPos)));
-            end
-        end
-    else
-     areas_interest_R = [53 54 12106 12107 12108 12109 12110 12123 49];
-     area_names = {'hippocampus','amygdala','ant cing','mid ant cing','mid post cing','post dorsal cing','post ventral cing','parahippocampal gyrus','thalamus'};
-        channel_areas = zeros(size(all_out(ss).channel_names));
-        stim_areas = zeros(length(all_out(ss).average_ccep_names),2);
-        % loop through channel names to get channel areas
-        for kk = 1:length(all_out(ss).channel_names)
-            % which number has this channel in the electrodes.tsv file?
-            thisElPos = find(ismember(electrodes_table.name,all_out(ss).channel_names{kk}));
-            if ismember(electrodes_table.Destrieux_label(thisElPos),areas_interest_R)
-                channel_areas(kk) = find(ismember(areas_interest_R,electrodes_table.Destrieux_label(thisElPos)));
+    % Get sites that belong to the stimulated ROI (stim_area)
+    these_stim_sites = find(ismember(all_out(ss).average_ccep_areas(:,1),area_codes{1}) | ...
+        ismember(all_out(ss).average_ccep_areas(:,2),area_codes{1}));
+    
+    % p-values for correction of multiple comparisons
+    all_out(ss).hasdata = NaN(size(all_out(ss).crp_out));
+    all_out(ss).crp_p = NaN(size(all_out(ss).crp_out));
+    all_out(ss).a_prime = NaN(size(all_out(ss).crp_out));
+    all_out(ss).cod = NaN(size(all_out(ss).crp_out));
+    all_out(ss).crp_p_adj = NaN(size(all_out(ss).crp_out));
+    all_out(ss).h = NaN(size(all_out(ss).crp_out));
+    all_out(ss).avg_trace_tR = zeros(size(all_out(ss).average_ccep));
+    
+    % loop over measured sites
+    for kk = 1:length(these_measured_sites)
+        % loop over the stimulated pairs
+        for ll = 1:length(these_stim_sites)
+            if ~isempty(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).data) % ~ same stim/recording site
+                all_out(ss).hasdata(these_measured_sites(kk), these_stim_sites(ll)) = 1;
+                all_out(ss).crp_p(these_measured_sites(kk), these_stim_sites(ll)) = all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_projs.p_value_tR;
+                all_out(ss).a_prime(these_measured_sites(kk), these_stim_sites(ll)) = mean(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.al_p); % mean alpha prime across trials;
+                all_out(ss).cod(these_measured_sites(kk), these_stim_sites(ll)) = median(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.cod); % median across trials
+                sig_timepoints = find(all_out(ss).tt>0.015 & all_out(ss).tt<=all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.tR);
+                all_out(ss).avg_trace_tR(these_measured_sites(kk), these_stim_sites(ll),sig_timepoints) = all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.C;
+
+            else
+                all_out(ss).hasdata(these_measured_sites(kk), these_stim_sites(ll)) = 0;
             end
         end
     end
-
-    % loop through stim pairs
-    for kk = 1:length(all_out(ss).average_ccep_names)
-        if sum(ismember(all_out(ss).average_ccep_names{kk},'-'))==1 % 1 -
-            stim_areas(kk,1) = channel_areas(ismember(all_out(ss).channel_names,extractBefore(all_out(ss).average_ccep_names{kk},'-')));
-            stim_areas(kk,2) = channel_areas(ismember(all_out(ss).channel_names,extractAfter(all_out(ss).average_ccep_names{kk},'-')));
-        else % assume the second - if there is a - in the channel name
-            dash_in_name = find(ismember(all_out(ss).average_ccep_names{kk},'-'));
-            el1_name = all_out(ss).average_ccep_names{kk}(1:dash_in_name(2)-1);
-            el2_name = all_out(ss).average_ccep_names{kk}(dash_in_name(2)+1:end);
-            stim_areas(kk,1) = channel_areas(ismember(channel_names,el1_name));
-            stim_areas(kk,2) = channel_areas(ismember(channel_names,el2_name));
-        end
-    end
-    stim_area = max(stim_areas,[],2); % take the maximum area between the two stim pairs, ignoring zeros, preferring second area
-    
-    all_out(ss).stim_area = stim_area;
-    all_out(ss).channel_areas = channel_areas;
-    %%% --> now we have channel_areas and stim_areas
-    
-    clear stim_area channel_area average_ccep average_ccep_names tt srate cod_ccep cod_ccep_p cod_ccep_quart
+    pvals = all_out(ss).crp_p(all_out(ss).hasdata==1);
+    qq = 0.05;
+    [h, crit_p, adj_ci_cvrg, adj_p] = fdr_bh(pvals,qq,'pdep','no');
+    all_out(ss).crp_p_adj(all_out(ss).hasdata==1) = adj_p;
+    all_out(ss).h(all_out(ss).hasdata==1) = h;
 end
 
 
@@ -125,65 +87,76 @@ end
 % rgb_color = {[0 0.4470 0.7410],[0.8500 0.3250 0.0980],[0.4660 0.6740 0.1880],[0.4940 0.1840 0.5560],[0.9290 0.6940 0.1250],[0.3010 0.7450 0.9330],[0.6350 0.0780 0.1840]};
 % blue, orange, green, purple, mustard, celeste, wine
 
-figure('Position',[0 0 500 350]), hold on; %('Position',[0 0 600 200]), hold on
+area_codes_r = {[12123 53],[54],[12108 12109 12110],[12106 12107],[59]}; % right
+area_codes_l = {[11123 17],[18],[11108 11109 11110],[11106 11107],[10]}; % left
+area_names = {'Hipp','Amyg','PCC','ACC','ANT'};   
 
-stim_area = 1; % 1 for hippocampus, 2 for amygdala, 3 for thalamus
+sub_hemi = {'r','r','r','l','r','l','l','r'};
+
+figure('Position',[0 0 500 350]), hold on; %('Position',[0 0 600 200]), hold on
 
 out_plot_responses_norm = [];
 out_subj_ind = [];
 resp_counter = 0;
-for ss = 4 %1:7
-% for ss = bids_sub
 
-    these_measured_sites = find(all_out(ss).channel_areas==5 | all_out(ss).channel_areas==6 | all_out(ss).channel_areas==7); % posterior cing
-    if stim_area==1
-        these_stim_sites = find(all_out(ss).stim_area==1 | all_out(ss).stim_area==8); % hippocampal formation 
-    elseif stim_area==2
-        these_stim_sites = find(all_out(ss).stim_area==2); % amygdala
-    elseif stim_area==3
-        these_stim_sites = find(all_out(ss).stim_area==9); % thalamus
-    end
-  
-    plot(all_out(ss).tt, ss + zeros(size(all_out(ss).tt)),'k')
-    
-    sign_resp = all_out(ss).sig_ccep==1; % plot p<0.05 FDR corrected
-    
-    % walk through pairs, test significance and plot if so
-    for kk = 1:length(these_measured_sites)
-        for ll = 1:length(these_stim_sites)
-            if sign_resp(these_measured_sites(kk),these_stim_sites(ll))==1
-                plot_responses = squeeze(all_out(ss).average_ccep(these_measured_sites(kk),these_stim_sites(ll),:));
-                plot_responses(all_out(ss).tt>-0.010 & all_out(ss).tt<0.010) = NaN;
-                
-                % Normalize. Scaling to unit length (Euclidean lenght): https://en.wikipedia.org/wiki/Feature_scaling
-                % unit length taken in same window as stats
-                response_vector_length = sum(plot_responses(all_out(ss).tt > t_win_cod(1) &  all_out(ss).tt < t_win_cod(2)) .^ 2) .^ .5;
-                plot_responses_norm = plot_responses ./ (response_vector_length*ones(size(plot_responses))); % normalize (L2 norm) each trial
+ss = 5;
 
-                % we looked at the data and visually split out 2 measure
-                % sites that produce different signatures
-                if kk==1 % measurement el 1 (RY1/MPC)
-                    plot(all_out(ss).tt, ss + plot_responses_norm,'color',[0 0.4470 0.7410 ],'LineWidth',.85)
-                    disp(['blue ' all_out(ss).channel_names(these_measured_sites(kk))])
-                elseif kk==2 % measurement el 2 (RY2/MPC)
-                    plot(all_out(ss).tt, ss + plot_responses_norm,'color',[1 .8 .1],'LineWidth',.85)
-                    disp(['yellow ' all_out(ss).channel_names(these_measured_sites(kk))])
-                elseif kk==3 % measurement el 3 ((RZ1/PDC)
-                    plot(all_out(ss).tt, ss + plot_responses_norm,'color',[.8 .8 .8],'LineWidth',.85)
-                    disp(['gray ' all_out(ss).channel_names(these_measured_sites(kk))])
+if isequal(sub_hemi{ss},'l')
+    area_codes = area_codes_l;
+elseif isequal(sub_hemi{ss},'r')
+    area_codes = area_codes_r;
+end
+
+stim_ind = 1;
+measure_ind = 3;
+
+these_measured_sites = find(ismember(all_out(ss).channel_areas,area_codes{measure_ind}));
+% Get sites that belong to the stimulated ROI (stim_area)
+these_stim_sites = find(ismember(all_out(ss).average_ccep_areas(:,1),area_codes{stim_ind}) | ...
+    ismember(all_out(ss).average_ccep_areas(:,2),area_codes{stim_ind}));
+
+plot(all_out(ss).tt, ss + zeros(size(all_out(ss).tt)),'k')
+
+sign_resp = all_out(ss).crp_p_adj<0.05; % plot p<0.05 FDR corrected
+
+t_win_cod = [0.015 0.500]; % window for vector length normalization and plotting across subjects
+
+% walk through pairs, test significance and plot if so
+for kk = 1:length(these_measured_sites)
+    for ll = 1:length(these_stim_sites)
+        if sign_resp(these_measured_sites(kk),these_stim_sites(ll))==1
+            plot_responses = squeeze(all_out(ss).average_ccep(these_measured_sites(kk),these_stim_sites(ll),:));
+            plot_responses(all_out(ss).tt>-0.010 & all_out(ss).tt<0.010) = NaN;
+            
+            % Normalize. Scaling to unit length (Euclidean lenght): https://en.wikipedia.org/wiki/Feature_scaling
+            % unit length taken in same window as stats
+            response_vector_length = sum(plot_responses(all_out(ss).tt > t_win_cod(1) &  all_out(ss).tt < t_win_cod(2)) .^ 2) .^ .5;
+            plot_responses_norm = plot_responses ./ (response_vector_length*ones(size(plot_responses))); % normalize (L2 norm) each trial
+
+            % we looked at the data and visually split out 2 measure
+            % sites that produce different signatures
+            if kk==1 % measurement el 1 (RY1/MPC)
+                plot(all_out(ss).tt, ss + plot_responses_norm,'color',[0 0.4470 0.7410 ],'LineWidth',.85)
+                disp(['blue ' all_out(ss).channel_names(these_measured_sites(kk))])
+            elseif kk==2 % measurement el 2 (RY2/MPC)
+                plot(all_out(ss).tt, ss + plot_responses_norm,'color',[1 .8 .1],'LineWidth',.85)
+                disp(['yellow ' all_out(ss).channel_names(these_measured_sites(kk))])
+            elseif kk==3 % measurement el 3 ((RZ1/PDC)
+                plot(all_out(ss).tt, ss + plot_responses_norm,'color',[.8 .8 .8],'LineWidth',.85)
+                disp(['gray ' all_out(ss).channel_names(these_measured_sites(kk))])
 %                 elseif kk==4 % measurement el 3 ((RZ1/PDC)
 %                     plot(all_out(ss).tt, ss + plot_responses_norm,'color',[1 .8 .1],'LineWidth',.85)
 %                     disp(['yellow ' all_out(ss).channel_names(these_measured_sites(kk))])
-                end
-                
-                % save outputs
-                resp_counter = resp_counter+1;
-                out_plot_responses_norm(resp_counter,:) = plot_responses_norm;
-                out_subj_ind(resp_counter,:) = ss;
             end
+            
+            % save outputs
+            resp_counter = resp_counter+1;
+            out_plot_responses_norm(resp_counter,:) = plot_responses_norm;
+            out_subj_ind(resp_counter,:) = ss;
         end
     end
 end
+
 
 title('Blue=RY1;Yellow=RY2')
 xlim([-0.2 .6])%, ylim([-2000 4000]);
